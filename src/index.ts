6914ee8b98982fabff4c04862717d141f375df45
Seamoon Pandey <hackedasocial@gmail.com>
2024-05-21 01:06:08 +0545

    - add and setup  husky - add and setup prettier - add and setup eslint - config ts - init eslintric.json

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Express + Ts Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + Ts Server");
 });
 
-app.listen(port, ()=>{
-	console.log(`[server]: Server s running at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`[server]: Server s running at http://localhost:${port}`);
 });
